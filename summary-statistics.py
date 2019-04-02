e129a8c49b19b1df68beb1e9a376859afa4a34d8
PAUL CAULFIELD <SESA87346@se.com>
2019-04-02 22:04:47 +0100

    fixed Invalid result ie nan using numpy.mean, replacing with numpy.nanmean, which calculates the mean ignoring the NaN's

--- summary-statistics.py
+++ summary-statistics.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 data = numpy.genfromtxt('iris.csv', delimiter=',')
 
 sepal_length = data[:,0]
-meansepal_length = numpy.mean(data[:,0])
+meansepal_length = numpy.nanmean(data[:,0])
+# numpy.nanmean used to calculate the mean, ignoring NaNs.) source https://stackoverflow.com/q/51720631
+# NaN means "Not a Number"; a flag to which indicates that the "operation's operands lie outside its domain" https://people.eecs.berkeley.edu/~wkahan/ieee754status/IEEE754.PDF
 
 print("Average of sepal_length is: ", meansepal_length)
 
