7e35b38407213e80353cb991dff399fea5e1372c
PAUL CAULFIELD <SESA87346@se.com>
2019-04-02 21:44:04 +0100

    added simple plot to visualize the data in sepal_length field

--- summary-statistics.py
+++ summary-statistics.py
@@ -2,10 +2,15 @@
 # My program reads in the iris.csv file and calculates some summary statistics.
 
 import numpy
+import matplotlib.pyplot as plt
 
 data = numpy.genfromtxt('iris.csv', delimiter=',')
 
-firstcol = data[:,0]
+sepal_length = data[:,0]
 meansepal_length = numpy.mean(data[:,0])
 
 print("Average of sepal_length is: ", meansepal_length)
+
+plt.hist(sepal_length)
+
+plt.show()
