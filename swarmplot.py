2ad75cdf02a01ce0024a65deb9a06d14c09310ea
PAUL CAULFIELD <SESA87346@se.com>
2019-04-10 22:20:11 +0100

    adapted code taken from https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#load

--- swarmplot.py
+++ swarmplot.py
@@ -1,3 +1,16 @@
 # # Paul Caulfield, 2019
 # My program reads in the iris.csv file and creates a swarmplot 
 # A swarmplot is a scatterplot with non-overlapping points: ref https://seaborn.pydata.org/generated/seaborn.swarmplot.html
+
+# Import necessary libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load iris data
+iris = sns.load_dataset("iris")
+
+# Construct iris plot
+sns.swarmplot(x="species", y="sepal_length", data=iris)
+
+# Show plot
+plt.show()
