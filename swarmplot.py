c6f2d515da781cc9c71ac7953a436ebf5c550d00
PAUL CAULFIELD <SESA87346@se.com>
2019-04-10 22:13:17 +0100

    create draft swarmplot

---
+++ swarmplot.py
@@ -0,0 +1,3 @@
+# # Paul Caulfield, 2019
+# My program reads in the iris.csv file and creates a swarmplot 
+# A swarmplot is a scatterplot with non-overlapping points: ref https://seaborn.pydata.org/generated/seaborn.swarmplot.html
