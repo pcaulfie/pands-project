4bcce05eabe3326c074d7d783c33ca1212d7b6f8
PAUL CAULFIELD <SESA87346@se.com>
2019-04-10 22:02:46 +0100

    adding additional comments to explain style = ticks

--- Scatterplot_Matrix.py
+++ Scatterplot_Matrix.py
@@ -2,14 +2,16 @@
 # My program reads in the iris.csv file and creates a scatterplot matrix.
 
 # Import necessary libraries adapted from https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#load
+# Matplotlib still underlies Seaborn, so that is why it is imported here; https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#load
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Solution https://seaborn.pydata.org/examples/scatterplot_matrix.html#
 
-# Select preset seaborn themes: ticks, used to give extra structure to the plots https://seaborn.pydata.org/tutorial/aesthetics.html
+# Solution adapted from https://seaborn.pydata.org/examples/scatterplot_matrix.html#
+
+# Seaborn does not have a default style, instead it comes with 5 preset styles, which must be called
+# I am using seaborn themes: ticks, as it is commonly used to give extra structure to the plots https://seaborn.pydata.org/tutorial/aesthetics.html
 sns.set(style="ticks")
-
 
 # Load iris data  as "df" from built-in Seaborn dataset located at https://github.com/mwaskom/seaborn-data/blob/master/iris.csv
 df = sns.load_dataset("iris")
