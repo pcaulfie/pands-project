fa72ad8f614c02f354a30018dd3e47f42a8b5f0e
PAUL CAULFIELD <SESA87346@se.com>
2019-04-10 22:38:48 +0100

    trying to create subplots which display 4 swarm plots side by side

--- swarmplot.py
+++ swarmplot.py
@@ -10,7 +10,14 @@ import matplotlib.pyplot as plt
 iris = sns.load_dataset("iris")
 
 # Construct iris plot
-sns.swarmplot(x="species", y="sepal_length", data=iris)
+plt.figure(figsize=(15,10))
+# Use subplot to compare different views of data side by side
+f, (ax1, ax2, ax3, ax4) = plt.subplot(2,2,1)
+
+ax1.sns.swarmplot(x="species", y="petal_length", data=iris)
+ax2.sns.swarmplot(x="species", y="petal_width", data=iris)
+ax3.sns.swarmplot(x="species", y="sepal_length", data=iris)
+ax4.sns.swarmplot(x="species", y="sepal_width", data=iris)
 
 # Show plot
 plt.show()
