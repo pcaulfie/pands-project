0aa399419104015514ba47dfd6094ab659734ba7
PAUL CAULFIELD <SESA87346@se.com>
2019-04-13 23:17:15 +0100

    Created a program to display ANOVA table

---
+++ anova.py
@@ -0,0 +1,9 @@
+# # Paul Caulfield, 2019
+# My program creates an ANOVA table in python using the Iris dataset. 
+
+# Solution adapted from https://www.kaggle.com/morenoh149/iris-anova-table-python
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+df = pd.read_csv('Iris.csv')
+df.head()
