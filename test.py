ddd79370909ddc0bd927162a201a6bf6650596b9
PAUL CAULFIELD <SESA87346@se.com>
2019-04-25 21:01:20 +0100

    added some comments and renamed variables

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # Paul Caulfield, 2019
-# My program will test some hypotheses 
+# My program will filter data using petal length to identify species with petal length less than 2cm
+# I subtracted the standard deviation from the mean to get 2 as the filter value.
 
 # Solution adapted from https://www.datacamp.com/community/tutorials/exploratory-data-analysis-python
 
@@ -9,8 +10,8 @@ import pandas as pd
 # making data frame from csv file  
 data = pd.read_csv("iris.csv") 
 
-# Filter data to Petal length < 2?
-a = data.query('petal_length < 2')
+# Filter data where Petal length < 2?
+result = data.query('petal_length < 2')
 
 # display
-print(a)
+print(result)
