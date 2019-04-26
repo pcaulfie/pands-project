f3fbc7e5fcca1273ebd41bf558c99be25141b2df
PAUL CAULFIELD <SESA87346@se.com>
2019-04-26 21:07:18 +0100

    added comments and references

--- pivot.py
+++ pivot.py
@@ -10,11 +10,14 @@ import numpy as np
 # adapted from https://www.datacamp.com/community/tutorials/exploratory-data-analysis-python
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv", delimiter=',')
 
-#create a pivot tables
+#create pivot tables to display mean and standard deviation using species as the index (rows) 
+# Code adapted from https://towardsdatascience.com/python-for-data-science-from-scratch-part-ii-e4dd4b943aba
 table = pd.pivot_table(df,values = ['petal_length','petal_width','sepal_length','sepal_width'], index=['species'],aggfunc=np.mean)
 table2 = pd.pivot_table(df,values = ['petal_length','petal_width','sepal_length','sepal_width'], index=['species'],aggfunc=np.std)
 # display results
+# Title Pivot Table 1
 print("Pivot Table - Mean")
 print(table)
+# Title Pivot Table 2
 print("Pivot Table - Standard Deviation")
 print(table2)
