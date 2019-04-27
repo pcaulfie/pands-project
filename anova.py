4f74fffbcbede5c3e03e3651641c0ed858ba4790
PAUL CAULFIELD <SESA87346@se.com>
2019-04-27 13:39:29 +0100

    formatted output for Two Way Anova

--- anova.py
+++ anova.py
@@ -4,30 +4,36 @@
 # Solution adapted from https://www.kaggle.com/morenoh149/iris-anova-table-python
 # Import the `pandas` library as `pd`
 import pandas as pd
-# Import the `pandas` library as `pd`
+# Import the `statsmodels` library as `sm`
 import statsmodels.api as sm
-#
+# Import OLS regression library
 from statsmodels.formula.api import ols
-## Load in the data with `read_csv()` then I pass the URL in which the data can be found. Column names are on the first row, so default is used. http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+# Load in the data with `read_csv()` then I pass the URL in which the data can be found. Column names are on the first row, so default is used. http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
 # adapted from https://www.datacamp.com/community/tutorials/exploratory-data-analysis-python
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv", delimiter=',')
 
+# create a one dimensional array using species as categorical and sepal length as variable dimension
 iris_lm_one_categorical=ols('sepal_length ~ C(species)', data=df).fit() 
 #Specify C for Categorical
+print("")
 print("One Way Anova - sepal_length")
+# Type 2 Anova dataframe
 print(sm.stats.anova_lm(iris_lm_one_categorical, typ=2))
 
 iris_lm_one_categorical2=ols('sepal_width ~ C(species)', data=df).fit() 
 #Specify C for Categorical
+print("")
 print("One Way Anova - sepal_width")
 print(sm.stats.anova_lm(iris_lm_one_categorical2, typ=2))
 
 iris_lm_one_categorical3=ols('petal_length ~ C(species)', data=df).fit() 
 #Specify C for Categorical
+print("")
 print("One Way Anova - petal_length")
 print(sm.stats.anova_lm(iris_lm_one_categorical3, typ=2))
 
 iris_lm_one_categorical4=ols('petal_width ~ C(species)', data=df).fit() 
 #Specify C for Categorical
+print("")
 print("One Way Anova - petal_width")
 print(sm.stats.anova_lm(iris_lm_one_categorical4, typ=2))
